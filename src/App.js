d4c13d7a060ec13a391d029e18423f00f9135068
mohammad606 <59096227+mohammad606@users.noreply.github.com>
2023-05-16 20:20:41 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import {Container} from "react-bootstrap"
+import NavbarA from "./components/NavbarA"
 
 function App() {
   return (
     <div className="color-body font">
       <NavbarA />
       <Container>
-        <HeadarA/>
-        <CatagoryA/>
-        <CardsA/>
       </Container>
     </div>
   );
